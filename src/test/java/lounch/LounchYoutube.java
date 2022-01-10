6e047b264d219aa18f1bf3470adfced22cc41060
rupendra <rupendras32@gmail.com>
2022-01-10 17:17:50 +0530

    commit repo

---
+++ src/test/java/lounch/LounchYoutube.java
@@ -0,0 +1,22 @@
+package lounch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LounchYoutube {
+	
+	WebDriver driver;
+	
+	@Test
+	public void openYoutube()
+	{
+	WebDriverManager.chromedriver().setup();
+	 driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.youtube.com/");
+	
+	}
+}
